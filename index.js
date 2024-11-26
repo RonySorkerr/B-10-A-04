0576cf2dbab622f8208f7c8eb4ac7b1fb6aa613e
RonySorkerr <mdronysarkar33299@gmail.com>
2024-11-26 13:37:00 +0600

    problem 5 done

--- index.js
+++ index.js
@@ -60,3 +60,27 @@ function calculateFinalScore(obj) {
     return finalScore >= 80;
 
 }
+
+function waitingTime(waitingTimes, serialNumber) {
+    if (!Array.isArray(waitingTimes) || typeof serialNumber !== 'number') {
+        return "Invalid Input";
+    }
+
+    let totalTime = 0;
+    for (let i = 0; i < waitingTimes.length; i++) {
+        totalTime += waitingTimes[i];
+    }
+    let averageTime = totalTime / waitingTimes.length;
+    averageTime = Math.round(averageTime);
+
+    let peopleAhead = serialNumber - 1;
+
+    let peopleRem = peopleAhead - waitingTimes.length;
+
+    let waitTime = 0;
+    if (peopleRem > 0) {
+        waitTime = peopleRem * averageTime;
+    }
+
+    return waitTime;
+}
